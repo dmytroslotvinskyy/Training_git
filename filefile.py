ee6dde096da5f2cc84a13f0972a38e709037e783
YaRa1312 <superfrench041@gmail.com>
2022-02-19 13:02:43 +0200

    new ficheare

--- filefile.py
+++ filefile.py
@@ -1,2 +1,20 @@
+import shutil
 print("Hello, World!")
 print("Hello, dear Dmytro!")
+def newfun2ction():
+    print ('f')
+    print ('u')
+    print ('c')
+    print ('k')
+
+
+def copyfiles(original, target):
+   # shutil.copyfile(original, target)
+    shutil.copy2(original, target)
+    print("Hello, " + name + ". Good morning!")
+
+
+newfun2ction()
+original = r'C:\Users\Ирина\Downloads\Cossacks European Wars'
+target = r'D:\new'
+copyfiles(original, target)
